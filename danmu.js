0cc688f474e1091d16ffe506a664968d62b4c78b
王贺 <33888579+haitang10@users.noreply.github.com>
2018-07-26 17:36:56 +0800

    Initial commit

---
+++ danmu.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+var log = function() {
+  console.log.apply(console, arguments)
+}
+
+var dm_text = document.querySelector('.dm_text')
+dm_text.addEventListener('')
+
+var dm_sub = document.querySelector('.dm_sub')
+dm_sub.addEventListener('click',function() {
+
+})
